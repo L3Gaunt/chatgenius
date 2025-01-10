f1ad0d342891b17b5cf71e0635ded1b67f1055af
LetterGaunt <193929321+L3Gaunt@users.noreply.github.com>
2025-01-10 20:39:11 +0100

    client side chat area

--- app/components/chat-area.tsx
+++ app/components/chat-area.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState, useRef, useEffect } from 'react'
 import { Send, Smile, MessageSquare, X, Trash2, Paperclip } from 'lucide-react'
 import { Button } from "@/components/ui/button"
